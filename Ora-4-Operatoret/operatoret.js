72728c5575236afa52a143641b4985f8c62091d4
Riana Fejzullahu <rianafejzullahu0@gmail.com>
2026-09-16 17:25:00 +0200

    mesimi i operatoreve

---
+++ Ora-4-Operatoret/operatoret.js
@@ -0,0 +1,45 @@
+console.log(10 + 5); // addition
+console.log(10 - 5); // substraction
+console.log(10 * 5); // multiplication
+console.log(10 / 5); // division
+console.log(10 % 3); // modulo
+console.log(10 ** 3); // fuqia
+
+
+// BMI = masa / gjatesia ** 2
+// BMI = masa / gjatesia * gjatesia
+
+let masaA = 78;
+let gjatesiaA = 1.80;
+
+let masaD = 92;
+let gjatesiaD = 1.95;
+
+let BMI_A = masaA / gjatesiaA * gjatesiaA;
+let BMI_D = masaD / gjatesiaD * gjatesiaD;
+
+console.log(BMI_A);
+console.log(BMI_D); 
+
+
+// Operatoret Logjike
+
+let vlera = true; // boolean
+console.log(true || false); // ose
+console.log(true && false); // dhe
+console.log(!true); // jo/not 
+
+
+// Operatoret Krahasues
+
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(5 <= 5);
+console.log(5 >= 5);
+
+let var1 = "5";
+let var2 = 5;
+
+console.log(var1 == var2);
+console.log(var1 === var2);
+console.log(var2 !== var3);
